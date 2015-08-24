d0a07b7d403f54efb02530239c6f01407359bf36
jaima <dekar.own@gmail.com>
2015-08-24 17:38:31 +0300

    First steps

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@
 
 lume = require "lume"
 lovebird = require "lovebird"
+tween = require "tween"
 
 function love.load()
 end
@@ -11,8 +12,13 @@ function love.update()
     print "to console?"
 end
 
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
+end
 function love.draw()
     for i=1, 10, 0.1 do
-       love.graphics.print('Hello World!', 400 + 150 * math.cos(i), 300 + 150 * math.sin(i))
+       love.graphics.print("Hello World!", 400 + 150 * math.cos(i), 300 + 150 * math.sin(i))
     end
 end
