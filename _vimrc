5f87a1722e72a569d4cc8bd767ccedb371a76af7
jaima <dekar.own@gmail.com>
2015-08-25 00:07:26 +0300

    vim script with hotkeys for running game

---
+++ _vimrc
@@ -0,0 +1,1 @@
+﻿map <F9> :silent call system('c:\dev\love-0.9.2-win64\love.exe c:\dev\nback')<CR>
--- main.lua
+++ main.lua
@@ -4,34 +4,42 @@ lume = require "lume"
 lovebird = require "lovebird"
 tween = require "tween"
 
-menu = {}
+menu = {
+    active_color = {255, 255, 255, 255},
+    inactive_color = {100, 200, 70, 255},
+}
 
 function menu.load()
     menu.font = love.graphics.newFont("gfx/DejaVuSansMono.ttf", 24)
     menu.items = {"play", "view progress", "quit"}
+    menu.active_index = 0
 end
 
 function menu.update()
 end
 
 function menu.draw()
+    --for setFont() and colors
+    love.graphics.push("all")
+
     w, h = love.graphics.getDimensions()
+    y = (h - #menu.items * menu.font:getHeight()) / 2
 
-    y = (h - #menu.items * menu.font.getHeight()) / 2
-
-    print "OK"
-    for i, k in ipairs(menu.items) do
-        x = (w - menu.font.getWidth()) / 2
-        love.graphics.setFont(menu.font)
-        print(k, x, y)
-        love.graphics.print(k, x, y)
-        y = y + menu.font.getHeight()
-    end
-end
-
-function menu.draw()
     love.graphics.setFont(menu.font)
-    love.graphics.print("ou!", 10, 10)
+
+    for i, k in ipairs(menu.items) do
+        x = (w - menu.font:getWidth(k)) / 2
+        if (menu.active_index == i) then
+            love.graphics.setColor(menu.inactive_color)
+            print "eq!"
+        else
+            love.graphics.setColor(menu.active_color)
+        end
+        love.graphics.print(k, x, y)
+        y = y + menu.font:getHeight()
+    end
+
+    love.graphics.pop()
 end
 
 function love.load()
