202ae1e421c1767bc8fcff606ad51f464d316a4c
jaima <dekar.own@gmail.com>
2015-08-24 17:24:08 +0300

    new content

---
+++ main.lua
@@ -0,0 +1,18 @@
+﻿require "math"
+
+lume = require "lume"
+lovebird = require "lovebird"
+
+function love.load()
+end
+
+function love.update()
+    lovebird.update()
+    print "to console?"
+end
+
+function love.draw()
+    for i=1, 10, 0.1 do
+       love.graphics.print('Hello World!', 400 + 150 * math.cos(i), 300 + 150 * math.sin(i))
+    end
+end
