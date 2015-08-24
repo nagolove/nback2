dca6ce2267c5a5353cf917071b3f9531547784e1
jaima <dekar.own@gmail.com>
2015-08-24 23:44:16 +0300

    Regular game stuff

--- main.lua
+++ main.lua
@@ -4,12 +4,45 @@ lume = require "lume"
 lovebird = require "lovebird"
 tween = require "tween"
 
+menu = {}
+
+function menu.load()
+    menu.font = love.graphics.newFont("gfx/DejaVuSansMono.ttf", 24)
+    menu.items = {"play", "view progress", "quit"}
+end
+
+function menu.update()
+end
+
+function menu.draw()
+    w, h = love.graphics.getDimensions()
+
+    y = (h - #menu.items * menu.font.getHeight()) / 2
+
+    print "OK"
+    for i, k in ipairs(menu.items) do
+        x = (w - menu.font.getWidth()) / 2
+        love.graphics.setFont(menu.font)
+        print(k, x, y)
+        love.graphics.print(k, x, y)
+        y = y + menu.font.getHeight()
+    end
+end
+
+function menu.draw()
+    love.graphics.setFont(menu.font)
+    love.graphics.print("ou!", 10, 10)
+end
+
 function love.load()
+    lovebird.update()
+
+    menu.load()
 end
 
 function love.update()
     lovebird.update()
-    print "to console?"
+    menu.update()
 end
 
 function love.keypressed(key)
@@ -17,8 +50,7 @@ function love.keypressed(key)
         love.event.quit()
     end
 end
+
 function love.draw()
-    for i=1, 10, 0.1 do
-       love.graphics.print("Hello World!", 400 + 150 * math.cos(i), 300 + 150 * math.sin(i))
-    end
+    menu.draw()
 end
