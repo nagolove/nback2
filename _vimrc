2ee355330185bb7620b5678ab15b7696e6a14a46
jaima <dekar.own@gmail.com>
2015-08-25 17:15:29 +0300

    Application skeleton

--- _vimrc
+++ _vimrc
@@ -1,1 +1,1 @@
-﻿map <F9> :silent call system('c:\dev\love-0.9.2-win64\love.exe c:\dev\nback')<CR>
+﻿map <F9> :w<CR>:silent call system('c:\dev\love-0.9.2-win64\love.exe c:\dev\nback')<CR>
--- main.lua
+++ main.lua
@@ -4,25 +4,28 @@ lume = require "lume"
 lovebird = require "lovebird"
 tween = require "tween"
 
+current_state = {}
 menu = {
     active_color = {255, 255, 255, 255},
     inactive_color = {100, 200, 70, 255},
-    active_index = 1
+    active_index = 1,
+    background_color = {20, 40, 80, 255}
 }
 
 function menu.play()
     print("play")
+    current_state = nback
+end
+
+function menu.view_progress()
 end
 
 function menu.quit()
     love.event.quit()
 end
 
-function menu.view_progress()
-end
-
 function menu.load()
-    menu.font = love.graphics.newFont("gfx/DejaVuSansMono.ttf", 24)
+    menu.font = love.graphics.newFont("gfx/DejaVuSansMono.ttf", 32)
     menu.items = {"play", "view progress", "quit"}
     menu.actions = { menu.play, menu.view_progress, menu.quit }
 end
@@ -33,6 +36,8 @@ function menu.keypressed(key)
         menu.active_index = menu.active_index - 1
     elseif key == "down" and menu.active_index + 1 <= #menu.items then
         menu.active_index = menu.active_index + 1
+    elseif key == "escape" then
+        menu.quit()
     elseif key == "return" or key == " " then
         menu.actions[menu.active_index]()
     end
@@ -49,6 +54,8 @@ function menu.draw()
     y = (h - #menu.items * menu.font:getHeight()) / 2
 
     love.graphics.setFont(menu.font)
+    love.graphics.setBackgroundColor(menu.background_color)
+    love.graphics.clear()
 
     for i, k in ipairs(menu.items) do
         x = (w - menu.font:getWidth(k)) / 2
@@ -61,27 +68,50 @@ function menu.draw()
         y = y + menu.font:getHeight()
     end
 
+    --restore settings
     love.graphics.pop()
+end
+
+nback = {}
+
+function nback.load()
+end
+
+function nback.update()
+end
+
+function nback.quit()
+    current_state = menu
+end
+
+function nback.keypressed(key)
+    if key == "escape" then
+        nback.quit()
+    end
+end
+
+function nback.draw()
 end
 
 function love.load()
     lovebird.update()
+    love.window.setTitle("nback trainer!")
 
     menu.load()
+    nback.load()
+
+    current_state = menu
 end
 
 function love.update()
     lovebird.update()
-    menu.update()
+    current_state.update()
 end
 
 function love.keypressed(key)
-    menu.keypressed(key)
-    if key == "escape" then
-        love.event.quit()
-    end
+    current_state.keypressed(key)
 end
 
 function love.draw()
-    menu.draw()
+    current_state.draw()
 end
