1eea34e75cd6fdbf5847c3ae017a889c189c55a5
jaima <dekar.own@gmail.com>
2015-10-21 19:51:00 +0300

    Worn on pviewer chart drawing

--- pviewer.lua
+++ pviewer.lua
@@ -8,7 +8,7 @@ local pviewer = {
     scroll_tip_text = "For scrolling table use ↓↑ arrows",
     header_text = "", 
     data = {}, -- games history which loading from file
-    border = 80, --border in pixels for drawing chart
+    border = 80, --y axis border in pixels for drawing chart
 
     update = function() end,
 
@@ -24,6 +24,47 @@ function pviewer.load()
     print(inspect(pviewer.data))
 
     pviewer.rt = love.graphics.newCanvas(love.graphics.getDimensions())
+end
+
+function draw_chart2()
+
+    local deltax = 0
+    local g = love.graphics
+
+    function draw_column(color, func)
+
+        g.setFont(pviewer.font)
+        g.setColor(color)
+
+        local dx = 0
+        local y = 0
+        for k, v in ipairs(pviewer.data) do
+            local s = func(k, v)
+            dx = math.max(dx, pviewer.font:getWidth(s))
+            g.print(s, deltax, y)
+            y = y + pviewer.font:getHeight()
+        end
+        deltax = deltax + dx
+    end
+
+    function draw_slash()
+        draw_column(colors.header, function(k, v)
+            return " / "
+        end)
+    end
+
+    draw_column(colors.chart, function(k, v)
+        return string.format("%.2d.%.2d.%d", 
+        v.date.day,
+        v.date.month,
+        v.date.year)
+    end)
+    draw_slash()
+    draw_column(colors.chart, function(k, v)
+        return string.format("%.2d %%", v.stat.hits)
+    end)
+
+    return deltax
 end
 
 function draw_chart()
@@ -42,6 +83,19 @@ function draw_chart()
         for k, v in ipairs(pviewer.data) do
             s = "  /  "
             g.print(s, 0 + deltax, y)
+            y = y + pviewer.font:getHeight()
+        end
+    end
+
+    --[[
+    [ func =    function(k, v)
+                    return "string" .. k
+                end
+      where k, v - pviewer.data
+    ]]
+    function draw_column(func)
+        for k, v in ipairs(pviewer.data) do
+            g.print(func(k, v), deltax, y)
             y = y + pviewer.font:getHeight()
         end
     end
@@ -102,9 +156,9 @@ function pviewer.draw()
     --drawing chart
     g.setCanvas(pviewer.rt)
     pviewer.rt:clear()
-    draw_chart()
+    local chart_width = draw_chart2()
     g.setCanvas()
-    g.draw(pviewer.rt, (w - pviewer.font:getWidth("11.09.2015  /  00 %")) / 2, 0)
+    g.draw(pviewer.rt, (w - chart_width) / 2, pviewer.border)
     --
 
     g.pop()
