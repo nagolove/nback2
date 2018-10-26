e7124ec599d93e9b5c7836938ecffa36ad2aaed1
dekar <dekar.own@gmail.com>
2018-10-26 20:06:02 +0300

    Extending history table fields

--- nback/nback.lua
+++ nback/nback.lua
@@ -360,6 +360,10 @@ function nback.save_to_history()
     --print("history", inspect(history))
     local d = os.date("*t")
     table.insert(history, { date = d, 
+                            pos_pressed_arr = nback.pos_pressed_arr,
+                            form_pressed_arr = nback.form_pressed_arr,
+                            sound_pressed_arr = nback.sound_pressed_arr,
+                            color_pressed_arr = nback.color_pressed_arr,
                             time = os.time(d), 
                             stat = nback.statistic,
                             nlevel = nback.level,
