cd42219a73a409b96faae5f849fb7d15fce963a3
dekar <dekar.own@gmail.com>
2019-01-18 18:06:05 +0300

    Breaked color picker under mouse cursor.

--- nback/colorpicker.lua
+++ nback/colorpicker.lua
@@ -18,21 +18,25 @@ end
 -- button = 2 -- secondary button
 function colorpicker:mousepressed(x, y, button, istouch)
     if button == 1 then
-        local r, g, b, a
+        local r, g, b, a = 0.1, 0.2, 0.3, 1
+        local c = 0
         lg.captureScreenshot(function(imgdata)
             print(love.mouse.getPosition())
             print("imagedata:getDimensions()", imgdata:getDimensions())
-            print("colors", r, g, b, a)
             --print(string.format("%f %f %f %f", self.color[1], self.color[2], self.color[3], self.color[4]))
             r, g, b, a = imgdata:getPixel(love.mouse.getPosition()) 
+            --print("vv = ", inspect(imgdata:getPixel(love.mouse.getPosition())))
+            print("colors", r, g, b, a)
+            c = 1
             --print(string.format("%f %f %f %f", self.color[1], self.color[2], self.color[3], self.color[4]))
         end)
+        print("c = ", c)
         self.color[1], self.color[2], self.color[3] = r, g, b
         print("cc", r, g, b)
         self.rslider.value, self.gslider.value, self.bslider.value = 1, 0, 1
     elseif button == 3 then
         print("color copied to clpbrd")
-        love.system.setClipboardText(string.format("{%s, %s, %s, %s}", self.color[1], self.color[2], self.color[3], self.color[4]))
+        love.system.setClipboardText(string.format("{%s, %s, %s}", self.color[1], self.color[2], self.color[3]))
     end
 end
     
@@ -81,6 +85,8 @@ end
 
 function colorpicker:update(dt)
     self.color = {self.rslider.value, self.gslider.value, self.bslider.value}
+    --print("color = ", inspect(self.color))
+    --print(string.format("color[1] = %f color[2] = %f color[3] = %f", self.color[1], self.color[2], self.color[3]))
 end
 
 return colorpicker
