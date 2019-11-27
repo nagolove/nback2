5547f1664e98eee9808372cb4b6df071f2cf701b
nagolove <dekar.own@gmail.com>
2019-11-27 15:38:23 +0300

    setupmenu stuff.

--- setupmenu.lua
+++ setupmenu.lua
@@ -8,6 +8,7 @@
 --
 -- * вывод расчетного значения времени раунда(Почему "раунд"? Бокс что-ли?
 -- Попробуй заменить на время концентрации
+local inspect = require "libs.inspect"
 
 local menu = {}
 menu.__index = menu
@@ -18,43 +19,73 @@ function menu.new(font, color)
     local self = {
         font = font,
         color = color,
+        items = {},
+        activeIndex = 1,
     }
     return setmetatable(self, menu)
 end
-   
--- oninit - пустое значение(nil) - недопустимая ситуация?
--- onleft, onright равны nil - значение не регулируется?
-function menu.addItem(oninit, onleft, onright)
+
+local function checkTableMember(t, name)
+    if t[name] then
+        print("checking type")
+        assert(type(t[name]) == "function", 
+            string.format("Field t['%s'] should be function", name))
+    end
 end
 
-function menu.scrollUp()
+-- oninit - пустое значение(nil) - недопустимая ситуация?
+-- onleft, onright равны nil - значение не регулируется?
+-- t["oninit"]
+-- t["onleft"]
+-- t["onright"]
+-- t["onselect"]
+function menu:addItem(t)
+    print("addItem", inspect(t))
+
+    assert(t)
+    assert(t.oninit ~= nil) -- обязательное поле
+
+    checkTableMember(t, "oninit")
+    checkTableMember(t, "onleft")
+    checkTableMember(t, "onright")
+    checkTableMember(t, "onselect")
+
+    self.items[#self.items + 1] = t
+    local item = self.items[#self.items]
+    item.text = item.oninit()
+    assert(type(item.text == "string"), "oninit() should return string.")
+end
+
+-- может сделать лучше прокрутку списка клавишами по-кругу? В виде
+-- закольцованного списка?
+function menu:scrollUp()
     if self.activeIndex - 1 >= 1 then
         self.activeIndex = self.activeIndex - 1
     end
 end
 
-function menu.scroollDown()
+function menu:scrollDown()
     if self.activeIndex + 1 <= #self.items then
         self.activeIndex = self.activeIndex + 1
     end
 end
 
 -- тут изменение параметра в меньшую стророну
-function menu.leftPressed()
+function menu:leftPressed()
 end
 
 -- тут изменение параметра в большую строну
-function menu.rightPressed()
+function menu:rightPressed()
 end
 
 -- рисовать курсор на активной позиции
-function menu.drawCursor()
+function menu:drawCursor()
 end
 
 -- рисовать менюшку с выравниванием относительно прямоугольника x, y, w, h
 -- целевая задача: рисовка одной единственной менюшки, в центре экрана, с
 -- выравниманием по-центру прямоугольника.
-function menu.draw(x, y, w, h)
+function menu:draw(x, y, w, h)
     for k, v in pairs(self.items) do
     end
 end
