538465e979dc192f85dab97f01f066d7e54d99d1
nagolove <dekar.own@gmail.com>
2019-12-28 22:17:12 +0300

    New script for copying .love game archive to Dropbox.

---
+++ pack_zip.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+apack game.zip alignedlabels.lua \
+    background.lua \
+    bhupur.lua \
+    common.lua \
+    conf.lua \
+    dbg.lua \
+    generator.lua \
+    geo.lua \
+    gfx \
+    help.lua \
+    ihelp.lua \
+    inspect.lua \
+    kons.lua \
+    libs \
+    main.lua \
+    menu.lua \
+    nback.lua \
+    pallete.lua \
+    pviewer.lua \
+    serpent.lua \
+    setupmenu.lua \
+    sfx \
+    signal.lua \
+    splash.lua 
+
+mv game.zip ~/Dropbox/nback/game.love
