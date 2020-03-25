6fb7028d0ef3118e04b4baaf554a969c47d49f4c
nagolove <dekar.own@gmail.com>
2020-03-25 14:52:05 +0300

    commentary shit.

--- pkg_send.sh
+++ pkg_send.sh
@@ -14,12 +14,12 @@ MINGW*)
     # изображения из gooi/imgs/* все равно попадают в список файлов.
     find . -name "*.lua" -or -name "*.wav" -or -name "*.ttf" -or -name "*.png" ! -path "gooi/imgs/*" ! -name "correct-filelist.lua" > files.txt
     ./correct-filelist.lua files.txt
-    #cat files.txt
     /c/Program\ Files/7-Zip/7z.exe a $packagename @files_.txt
-    rm files.txt
-    rm files_.txt
     scp $packagename dekar@visualdoj.ru:/home/dekar/www/packages
     ssh dekar@visualdoj.ru /home/dekar/bin/update_index.lua
+
+    rm files.txt
+    rm files_.txt
     rm $packagename
     ;;
 *)          
