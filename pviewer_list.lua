e06b724dfb56d92d378172a3a932178c0d66b000
nagolove <dekar.own@gmail.com>
2020-04-01 22:01:42 +0300

    Exercising in touchscreen.

--- pviewer_list.lua
+++ pviewer_list.lua
@@ -1,6 +1,7 @@
 ----------------------
 -- List object
 ----------------------
+local pallete = require "pallete"
 
 local List = {}
 
@@ -29,10 +30,14 @@ function List:new(x, y, w, h)
     o.sum_item_height = 0
 
 	o.colors = {}
-	o.colors.normal = {bg = {0.19, 0.61, 0.88}, fg = {0.77, 0.91, 1}}
+	--o.colors.normal = {bg = {0.19, 0.61, 0.88}, fg = {0.77, 0.91, 1}}
+	o.colors.normal = {bg = {0, 0, 0}, fg = {0.77, 0.91, 1}}
 	o.colors.hover  = {bg = {0.28, 0.51, 0.66}, fg = {1, 1, 1}}
 	o.windowcolor = {0.19, 0.61, 0.88}
 	o.bordercolor = {0.28, 0.51, 0.66}
+    o.touches = {}
+    o.drawList = {}
+
     return o
 end
 
@@ -59,6 +64,7 @@ function List:done()
     self.bar.maxpos = self.height - self.bar.size - 3
     -- Calculate height of everything.
     self.sum_item_height = (self.item_height+1) * self.items.n + 2
+    self.bar.y = 0
     self:prepareDrawing()
 end
 
@@ -87,6 +93,29 @@ function List:update(dt)
         end
 
         self.bar.lock.y = love.mouse.getY()
+    end
+
+    local touchesSeq = {}
+    for k, v in pairs(self.touches) do
+        table.insert(touchesSeq, v)
+    end
+
+    local first = touchesSeq[1]
+    if #touchesSeq == 1 then
+        table.insert(self.drawList, function()
+            love.graphics.setColor{0.5, 0.5, 0.5, 0.5}
+            love.graphics.circle("fill", first.x, first.y, 15)
+        end)
+        if inside(first.x, first.y, self.x + 2, self.y + 1, self.width - 3, self.height - 3) then
+            local tx, ty = x - self.x, y + self:getOffset() - self.y
+            local index = math.floor((ty / self.sum_item_height) * self.items.n)
+            local item = self.items[index + 1]
+            if item then
+                print("onclick", index + 1)
+                self.onclick(item, index + 1, b)
+            end
+            self.active = index + 1
+        end
     end
 end
 
@@ -134,15 +163,30 @@ function List:mousemoved(x, y, dx, dy)
 	end
 end
 
-function List:wheelmoved(x, y)
-	if self:hasBar() then
-		local per_pixel = (self.sum_item_height - self.height) / self.bar.maxpos
-		local bar_pixel_dt = math.floor((self.item_height * 3) / per_pixel + 0.5)
+function List:touchpressed(id, x, y)
+    print(":touchpressed(id, x, y)")
+    self.touches[id] = {x = x, y = y}
+end
 
-		self.bar.pos = self.bar.pos - y * bar_pixel_dt
-		if self.bar.pos > self.bar.maxpos then self.bar.pos = self.bar.maxpos end
-		if self.bar.pos < 0 then self.bar.pos = 0 end
-	end
+function List:touchreleased(id, x, y)
+    print(":touchreleased(id, x, y)")
+    self.touches[id] = nil
+end
+
+function List:touchmoved(id, x, y, dx, dy)
+    self.touches[id] = {x = x, y = y, dx = dx, dy = dy}
+    print("touchmoved", id, x, y, dx, dy)
+end
+
+function List:wheelmoved(x, y)
+	--if self:hasBar() then
+		--local per_pixel = (self.sum_item_height - self.height) / self.bar.maxpos
+		--local bar_pixel_dt = math.floor((self.item_height * 3) / per_pixel + 0.5)
+
+		--self.bar.pos = self.bar.pos - y * bar_pixel_dt
+		--if self.bar.pos > self.bar.maxpos then self.bar.pos = self.bar.maxpos end
+		--if self.bar.pos < 0 then self.bar.pos = 0 end
+	--end
 end
 
 function List:getBarRect()
@@ -153,10 +197,10 @@ function List:getBarRect()
 end
 
 function List:getItemRect(i)
-    return self.x + self.bar.width, self.y + ((self.item_height + 1) * (i - 1) + 1) - self:getOffset(),
-        self.width - self.bar.width, self.item_height
-	--return self.x + 2, self.y + ((self.item_height + 1) * (i - 1) + 1) - self:getOffset(),
-		--self.width - 3, self.item_height
+    --return self.x + self.bar.width, self.y + ((self.item_height + 1) * (i - 1) + 1) - self:getOffset(),
+        --self.width - self.bar.width, self.item_height
+    return self.x + 2, self.y + ((self.item_height + 1) * (i - 1) + 1) - self:getOffset(),
+        self.width - 3, self.item_height
 end
 
 function List:draw()
@@ -175,16 +219,21 @@ function List:draw()
 	local rx, ry, rw, rh
 	local colorset
 	for i = start_i, end_i do
-		if i == self.hoveritem then
-			colorset = self.colors.hover
-		else
-			colorset = self.colors.normal
-		end
+		--if i == self.hoveritem then
+			--colorset = self.colors.hover
+		--else
+			--colorset = self.colors.normal
+		--end
 
 		rx, ry, rw, rh = self:getItemRect(i)
         local item = self.items[i]
         love.graphics.setColor{1, 1, 1, 1}
         love.graphics.draw(item.canvas, rx, ry)
+
+        if self.active == i then
+            love.setColor{0, 0, 0}
+            love.graphics("line", rx, ry, rw, rh)
+        end
         --love.graphics.setColor(colorset.bg)
         --love.graphics.rectangle("fill", rx, ry, rw, rh)
 
@@ -192,20 +241,17 @@ function List:draw()
         --love.graphics.print(self.items[i].title, rx + 10, ry + 5) 
 	end
 
-	--love.graphics.setScissor()
+    for k, v in pairs(self.drawList) do
+        v()
+    end
 
-	-- Bar
-	if self:hasBar() then
-		if self.hoveritem == -1 or self.bar.lock then
-			colorset = self.colors.hover
-		else
-			colorset = self.colors.normal
-		end
-		
-		rx, ry, rw, rh = self:getBarRect()
-		love.graphics.setColor(colorset.bg)
-		love.graphics.rectangle("fill", rx, ry, rw, rh)
-	end
+	--love.graphics.setScissor()
+    love.graphics.setColor(pallete.pviewer.scrollLine)
+	love.graphics.setLineWidth(2)
+	love.graphics.setLineStyle("smooth")
+    love.graphics.line(self.x + 2, self.y, self.x + 2, self.y + self.height)
+    love.graphics.setColor(pallete.pviewer.circle)
+    love.graphics.circle("fill", self.x + 2, self.bar.y, 3)
 end
 
 function List:prepareDrawing()
@@ -224,25 +270,56 @@ function List:prepareDrawing()
     local rx, ry, rw, rh
     local colorset
     for i = 1, self.items.n do
-        if i == self.hoveritem then
-            colorset = self.colors.hover
-        else
-            colorset = self.colors.normal
-        end
+        --if i == self.hoveritem then
+            --colorset = self.colors.hover
+        --else
+            --colorset = self.colors.normal
+        --end
+        colorset = self.colors.normal
         rx, ry, rw, rh = self:getItemRect(i)
         local item = self.items[i]
         item.canvas = love.graphics.newCanvas(rw, rh)
         love.graphics.setCanvas(item.canvas)
 
-        --love.graphics.setColor(colorset.bg)
+        love.graphics.setColor(colorset.bg)
         love.graphics.rectangle("fill", 0, 0, rw, rh)
 
+        --love.graphics.setColor{0, 0, 0}
+        --love.graphics.rectangle("line", 2, 2, rw - 2, rh - 2)
+        --love.graphics.rectangle("fill", 2, 2, rw - 2, rh - 2)
+
+        --g.print(compareDates(os.date("*t"), item.data.date), rx, ry)
+
         love.graphics.setColor(colorset.fg)
-        love.graphics.print(self.items[i].title, 10, 5) 
+        local str = compareDates(os.date("*t"), item.data.date)
+        --local str = compareDates(os.date("*t"), item.data.date) .. " " .. tostring(item.data.date.year * 365 * 24 + item.data.date.yday * 24 + item.data.date.hour)
+        --local str = " " .. tostring(item.data.date.year * 365 * 24 + item.data.date.yday * 24 + item.data.date.hour)
+        love.graphics.print(str, 10, 5) 
 
         love.graphics.setCanvas()
     end
 
+    local _, _, rw, rh = self:getItemRect(1)
+    local x, y, w, h = 1, 1, rw - 1, rh - 1
+    self.downCanvas = love.graphics.newCanvas(rw, rh)
+    love.graphics.setCanvas(self.downCanvas)
+    
+    love.graphics.setColor(pallete.pviewer.scrollTriangle)
+    love.graphics.polygon("fill", w / 2 - w / 3, y, w / 2, h, w / 2 + w / 3, y)
+
+    self.upCanvas = love.graphics.newCanvas(rw, rh)
+    love.graphics.setCanvas(self.upCanvas)
+
+    love.graphics.setColor(pallete.pviewer.scrollTriangle)
+    love.graphics.polygon("fill", w / 2 - w / 3, h, w / 2, y, w / 2 + w / 3, h)
+    love.graphics.setCanvas()
+
+    self:saveCanvases()
+end
+
+function List:saveCanvases()
+    self.upCanvas:newImageData():encode("png", "up.png")
+    self.downCanvas:newImageData():encode("png", "down.png")
     for i in ipairs(self.items) do
         local imageData = self.items[i].canvas:newImageData()
         local file = imageData:encode("png", string.format("item_%d.png", i))
