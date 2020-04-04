1b6ff15b7485b6cc91b2cf930b7ae8bd41b07b8d
nagolove <dekar.own@gmail.com>
2020-04-04 14:02:33 +0300

    coroprocessor. Some code removed.

---
+++ coroprocessor.lua
@@ -0,0 +1,53 @@
+﻿local inspect = require "inspect"
+local resume = coroutine.resume
+
+local CoroProcessor = {}
+CoroProcessor.__index = CoroProcessor
+
+function CoroProcessor.new()
+    local self = setmetatable({}, CoroProcessor)
+    self.coros = {}
+    self.messages = {}
+    return self
+end
+
+function CoroProcessor:sendMessage(queuename, message)
+    local tbl = self.messages[queuename] 
+    if tbl then
+        table.insert(tbl, message)
+    end
+end
+
+function CoroProcessor:push(queuename, func, ...)
+    local q = self.coros[queuename]
+    if not q then
+        self.coros[queuename] = {}
+        self.messages[queuename] = {}
+        q = self.coros[queuename]
+    end
+    table.insert(q, coroutine.create(func))
+end
+
+function CoroProcessor:update()
+    for k, v in pairs(self.coros) do
+        if #v >= 1 then
+            local msgs = self.messages[k]
+            local msg
+            if #msgs >= 1 then
+                msg = msgs[1]
+                table.remove(msgs, 1)
+            end
+            local ret
+            if msg then
+                ret = resume(v[1], msg)
+            else
+                ret = resume(v[1])
+            end
+            if not ret then
+                table.remove(v, 1)
+            end
+        end
+    end
+end
+
+return CoroProcessor
