e42554d53ed627203c89d86ca2b5bbf3a614dae3
nagolove <dekar.own@gmail.com>
2020-04-09 18:46:21 +0300

    Rewriting.

--- pviewer_list.lua
+++ pviewer_list.lua
@@ -15,18 +15,12 @@ function List:new(x, y, w, h)
     self.__index = self
 
     o.items = {}
-	o.hoveritem = 0
 	o.onclick = nil
 
-    o.x = x
-    o.y = y
+    o.x, o.y = x, y
+    o.width, o.height = w, h
 
-    o.bar = { size = 20, pos = 0, maxpos = 0, width = 15, lock = nil}
-
-    o.width = w - o.bar.width
-    o.height = h
-
-    o.item_height = 23
+    o.item_height = 23 -- где учет высоты шрифта?
     o.sum_item_height = 0
 
 	o.colors = {}
@@ -37,64 +31,35 @@ function List:new(x, y, w, h)
 	o.bordercolor = {0.28, 0.51, 0.66, 1}
     o.touches = {}
     o.drawList = {}
+    o.activeIndex = 0 -- никакой пунки не активный
 
     return o
 end
 
-function List:add(title, id)
+function List:add(title)
 	local item = {}
 	item.title = title
-	item.id = id
-	--if type(tooltip) == "string" then
-		--item.tooltip = tooltip
-	--else
-		--item.tooltip = ""
-	--end
     table.insert(self.items, item)
     return item
 end
 
 function List:done()
     self.items.n = #self.items
-    -- Recalc bar size.
-    self.bar.pos = 0
-    local num_items = (self.height / self.item_height)
-    local ratio = num_items / self.items.n
-    self.bar.size = self.height * ratio
-    self.bar.maxpos = self.height - self.bar.size - 3
+    self.visibleNum = self.height / self.item_height
+    --local num_items = (self.height / self.item_height)
     -- Calculate height of everything.
-    self.sum_item_height = (self.item_height+1) * self.items.n + 2
-    self.bar.y = 0
+    --self.sum_item_height = (self.item_height+1) * self.items.n + 2
+    self.start_i = 1
+    if #self.items > self.visibleNum then
+        self.end_i = self.visibleNum - 1
+        self.canDown = true
+    else
+        self.end_i = #self.items
+    end
     self:prepareDrawing()
 end
 
-function List:hasBar()
-    return self.sum_item_height > self.height
-end
-
-function List:getBarRatio()
-    return self.bar.pos / self.bar.maxpos
-end
-
-function List:getOffset()
-    local ratio = self.bar.pos / self.bar.maxpos
-    return math.floor((self.sum_item_height - self.height) * ratio + 0.5)
-end
-
 function List:update(dt)
-    if self.bar.lock then
-        local dy = math.floor(love.mouse.getY()-self.bar.lock.y+0.5)
-        self.bar.pos = self.bar.pos + dy
-
-        if self.bar.pos < 0 then
-            self.bar.pos = 0
-        elseif self.bar.pos > self.bar.maxpos then
-            self.bar.pos = self.bar.maxpos
-        end
-
-        self.bar.lock.y = love.mouse.getY()
-    end
-
     local touchesSeq = {}
     for k, v in pairs(self.touches) do
         table.insert(touchesSeq, v)
@@ -106,61 +71,40 @@ function List:update(dt)
             love.graphics.setColor{0.5, 0.5, 0.5, 0.5}
             love.graphics.circle("fill", first.x, first.y, 15)
         end)
-        if inside(first.x, first.y, self.x + 2, self.y + 1, self.width - 3, self.height - 3) then
-            local tx, ty = x - self.x, y + self:getOffset() - self.y
-            local index = math.floor((ty / self.sum_item_height) * self.items.n)
-            local item = self.items[index + 1]
-            if item then
-                print("onclick", index + 1)
-                self.onclick(item, index + 1, b)
-            end
-            self.active = index + 1
-        end
+        --if inside(first.x, first.y, self.x + 2, self.y + 1, self.width - 3, self.height - 3) then
+            --local tx, ty = x - self.x, y + self:getOffset() - self.y
+            --local index = math.floor((ty / self.sum_item_height) * self.items.n)
+            --local item = self.items[index + 1]
+            --if item then
+                --print("onclick", index + 1)
+                --self.onclick(item, index + 1, b)
+            --end
+            --self.active = index + 1
+        --end
     end
 end
 
 function List:mousepressed(x, y, b, it)
-	if b == 1 and self:hasBar() then
-		local rx, ry, rw, rh = self:getBarRect()
-		if inside(x, y, rx, ry, rw, rh) then
-			self.bar.lock = { x = x, y = y }
-			return
-		end
-	end
-
-	if type(self.onclick) == "function" and 
-    inside(x, y, self.x + 2, self.y + 1, self.width - 3, self.height - 3) then
-		local tx, ty = x - self.x, y + self:getOffset() - self.y
-		local index = math.floor((ty / self.sum_item_height) * self.items.n)
-		local item = self.items[index + 1]
-		if item then
-			self.onclick(item, index + 1, b)
-		end
-    end
+	--if type(self.onclick) == "function" and 
+        --inside(x, y, self.x + 2, self.y + 1, self.width - 3, self.height - 3) then
+            --local tx, ty = x - self.x, y + self:getOffset() - self.y
+            --local index = math.floor((ty / self.sum_item_height) * self.items.n)
+            --local item = self.items[index + 1]
+            --if item then
+                --self.onclick(item, index + 1, b)
+            --end
+    --end
 end
 
 function List:mousereleased(x, y, b, it)
-	if b == 1 and self:hasBar() then
-		self.bar.lock = nil
-	end
 end
 
 function List:mousemoved(x, y, dx, dy)
-	self.hoveritem = 0
-
-	if self:hasBar() then
-		local rx, ry, rw, rh = self:getBarRect()
-		if inside(x, y, rx, ry, rw, rh) then
-			self.hoveritem = -1
-			return
-		end
-	end
-
-	if inside(x, y, self.x + 2, self.y + 1, self.width - 3, self.height - 3) then
-		local tx, ty = x - self.x, y + self:getOffset() - self.y
-		local index = math.floor((ty / self.sum_item_height) * self.items.n)
-		self.hoveritem = index + 1
-	end
+	--if inside(x, y, self.x + 2, self.y + 1, self.width - 3, self.height - 3) then
+		--local tx, ty = x - self.x, y + self:getOffset() - self.y
+		--local index = math.floor((ty / self.sum_item_height) * self.items.n)
+		--self.hoveritem = index + 1
+	--end
 end
 
 function List:touchpressed(id, x, y)
@@ -179,28 +123,10 @@ function List:touchmoved(id, x, y, dx, dy)
 end
 
 function List:wheelmoved(x, y)
-	--if self:hasBar() then
-		--local per_pixel = (self.sum_item_height - self.height) / self.bar.maxpos
-		--local bar_pixel_dt = math.floor((self.item_height * 3) / per_pixel + 0.5)
-
-		--self.bar.pos = self.bar.pos - y * bar_pixel_dt
-		--if self.bar.pos > self.bar.maxpos then self.bar.pos = self.bar.maxpos end
-		--if self.bar.pos < 0 then self.bar.pos = 0 end
-	--end
-end
-
-function List:getBarRect()
-    return self.x, self.y + self.bar.pos + 1,
-        self.bar.width - 3, self.bar.size
-	--return self.x + self.width + 2, self.y + self.bar.pos + 1,
-		--self.bar.width - 3, self.bar.size
 end
 
 function List:getItemRect(i)
-    --return self.x + self.bar.width, self.y + ((self.item_height + 1) * (i - 1) + 1) - self:getOffset(),
-        --self.width - self.bar.width, self.item_height
-    return self.x + 2, self.y + ((self.item_height + 1) * (i - 1) + 1) - self:getOffset(),
-        self.width - 3, self.item_height
+    return self.x, self.y + self.item_height * (i - 1), self.width, self.item_height
 end
 
 function List:draw()
@@ -208,29 +134,17 @@ function List:draw()
 	love.graphics.setLineStyle("rough")
 	love.graphics.setColor(self.windowcolor)
 
-	-- Get interval to display
-	local start_i = math.floor(self:getOffset() / (self.item_height + 1)) + 1
-	local end_i = start_i + math.floor(self.height / (self.item_height + 1)) + 1
-	if end_i > self.items.n then end_i = self.items.n end
-
 	--love.graphics.setScissor(self.x, self.y, self.width, self.height)
 
-	-- Items
 	local rx, ry, rw, rh
 	local colorset
-	for i = start_i, end_i do
-		--if i == self.hoveritem then
-			--colorset = self.colors.hover
-		--else
-			--colorset = self.colors.normal
-		--end
-
+	for i = self.start_i, self.end_i do
 		rx, ry, rw, rh = self:getItemRect(i)
         local item = self.items[i]
         love.graphics.setColor{1, 1, 1, 1}
         love.graphics.draw(item.canvas, rx, ry)
 
-        if self.active == i then
+        if self.activeIndex == i then
             love.setColor{0, 0, 0}
             love.graphics("line", rx, ry, rw, rh)
         end
@@ -251,7 +165,7 @@ function List:draw()
 	love.graphics.setLineStyle("smooth")
     love.graphics.line(self.x + 2, self.y, self.x + 2, self.y + self.height)
     love.graphics.setColor(pallete.pviewer.circle)
-    love.graphics.circle("fill", self.x + 2, self.bar.y, 3)
+    --love.graphics.circle("fill", self.x + 2, self.bar.y, 3)
 end
 
 function List:prepareDrawing()
@@ -259,22 +173,9 @@ function List:prepareDrawing()
 	love.graphics.setLineStyle("rough")
 	love.graphics.setColor(self.windowcolor)
 
-	-- Get interval to display
-	local start_i = math.floor(self:getOffset() / (self.item_height + 1)) + 1
-	local end_i = start_i + math.floor(self.height / (self.item_height + 1)) + 1
-	if end_i > self.items.n then end_i = self.items.n end
-
-	love.graphics.setScissor(self.x, self.y, self.width, self.height)
-
-    -- Items
     local rx, ry, rw, rh
     local colorset
     for i = 1, self.items.n do
-        --if i == self.hoveritem then
-            --colorset = self.colors.hover
-        --else
-            --colorset = self.colors.normal
-        --end
         colorset = self.colors.normal
         rx, ry, rw, rh = self:getItemRect(i)
         local item = self.items[i]
@@ -282,21 +183,13 @@ function List:prepareDrawing()
         item.canvas = love.graphics.newCanvas(rw, rh)
         love.graphics.setCanvas(item.canvas)
 
-        love.graphics.clear(1, 1, 1, 1)
+        --[[love.graphics.clear(1, 1, 1, 1)]]
+        love.graphics.clear(0, 0, 0, 0)
         love.graphics.setColor(colorset.bg)
         love.graphics.rectangle("fill", 0, 0, rw, rh)
 
-        --love.graphics.setColor{0, 0, 0}
-        --love.graphics.rectangle("line", 2, 2, rw - 2, rh - 2)
-        --love.graphics.rectangle("fill", 2, 2, rw - 2, rh - 2)
-
-        --g.print(compareDates(os.date("*t"), item.data.date), rx, ry)
-
         love.graphics.setColor(colorset.fg)
-        local str = compareDates(os.date("*t"), item.data.date)
-        --local str = compareDates(os.date("*t"), item.data.date) .. " " .. tostring(item.data.date.year * 365 * 24 + item.data.date.yday * 24 + item.data.date.hour)
-        --local str = " " .. tostring(item.data.date.year * 365 * 24 + item.data.date.yday * 24 + item.data.date.hour)
-        love.graphics.print(str, 10, 5) 
+        love.graphics.print(item.title, 10, 5) 
 
         love.graphics.setCanvas()
     end
