84386afaef4560cce73e45eb4c42816f5fc5e13a
nagolove <dekar.own@gmail.com>
2021-02-11 20:24:24 +0300

    Circular module reference.

--- setupmenu.lua
+++ setupmenu.lua
@@ -1,313 +1,372 @@
---
--- * уровень н-назад
--- * временя паузы
--- * длина раунда
---
--- * вывод расчетного значения времени раунда(Почему "раунд"? Бокс что-ли?
--- Попробуй заменить на время концентрации
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local assert = _tl_compat and _tl_compat.assert or assert; local ipairs = _tl_compat and _tl_compat.ipairs or ipairs; local string = _tl_compat and _tl_compat.string or string
 
+
+
+
+
+
+
+require("love")
 require("common")
-local inspect = require "libs.inspect"
+
 local g = love.graphics
 
-local menu = {}
-menu.__index = menu
+ SetupMenu = {Rect = {}, ItemHandler = {}, }
 
--- парамертрами могут быть - шрифт и его размер, цвет текста, стиль рамки
--- выделения пункта
-function menu.new(font, color)
-    local self = {
-        font = font,
-        color = color,
-        items = {},
-        activeIndex = 1,
-        cursorLineWidth = 3,
-        cursorColor = {0.8, 0, 0},
-        markerColor = {1, 1, 1},
-        activeMarkerColor = {0, 0.8, 0},
-        inactiveMarkerColor = {0.5, 0.5, 0.5},
-    }
-    return setmetatable(self, menu)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+local SetupMenu_mt = {
+   __index = SetupMenu,
+}
+
+
+
+function SetupMenu.new(font, color)
+   local self = {
+      font = font,
+      color = color,
+      items = {},
+      activeIndex = 1,
+      cursorLineWidth = 3,
+      cursorColor = { 0.8, 0, 0 },
+      markerColor = { 1, 1, 1 },
+      activeMarkerColor = { 0, 0.8, 0 },
+      inactiveMarkerColor = { 0.5, 0.5, 0.5 },
+   }
+   return setmetatable(self, SetupMenu_mt)
 end
 
 local function checkTableMember(t, name)
-    if t[name] then
-        --print("checking type")
-        assert(type(t[name]) == "function", string.format("Field t['%s'] should be function", name))
-    end
+   if (t)[name] then
+
+      assert(type((t)[name]) == "function", string.format("Field t['%s'] should be function", name))
+   end
 end
 
--- oninit - пустое значение(nil) - недопустимая ситуация?
--- onleft, onright равны nil - значение не регулируется?
--- t["oninit"]
--- t["onleft"]
--- t["onright"]
--- t["onselect"]
-function menu:addItem(t)
-    --print("addItem", inspect(t))
 
-    assert(t)
-    assert(t.oninit ~= nil) -- обязательное поле
 
-    checkTableMember(t, "oninit")
-    checkTableMember(t, "onleft")
-    checkTableMember(t, "onright")
-    checkTableMember(t, "onselect")
 
-    self.items[#self.items + 1] = t
-    local item = self.items[#self.items]
-    item.content, item.isfirst, item.islast = item.oninit()
 
-    if not item.isfirst then
-        item.isfirst = false
-    end
-    if not item.islast then
-        item.islast = false
-    end
 
-    assert(type(item.content == "table"), "oninit() should return table.")
-    item.leftPressedKey = false
-    item.rightPressedKey = false
+
+function SetupMenu:addItem(t)
+
+
+   assert(t)
+   assert(t.oninit ~= nil)
+
+   checkTableMember(t, "oninit")
+   checkTableMember(t, "onleft")
+   checkTableMember(t, "onright")
+   checkTableMember(t, "onselect")
+
+   self.items[#self.items + 1] = t
+   local item = self.items[#self.items]
+   item.content, item.isfirst, item.islast = item.oninit()
+
+   if not item.isfirst then
+      item.isfirst = false
+   end
+   if not item.islast then
+      item.islast = false
+   end
+
+   assert(type(item.content) == "table", "oninit() should return table.")
+   item.leftPressedKey = false
+   item.rightPressedKey = false
 end
 
-function menu:select()
-    local item = self.items[self.activeIndex]
-    if item and item.onselect then
-        item:onselect()
-    end
+function SetupMenu:select()
+   local item = self.items[self.activeIndex]
+   if item and item.onselect then
+      item:onselect()
+   end
 end
 
-function menu:update(dt)
-    --print("menu:update()")
-    local item = self.items[self.activeIndex]
-    --linesbuf:pushi("item.leftPressedKey = %s", tostring(item.leftPressedKey))
-    --linesbuf:pushi("item.rightPressedKey = %s", tostring(item.rightPressedKey))
-    if item then
-        linesbuf:pushi("item.isfirst = %s, item.islast = %s", item.isfirst, item.islast)
-    end
+function SetupMenu:update(_)
+
+   local item = self.items[self.activeIndex]
+
+
+   if item then
+
+   end
 end
 
-function menu:scrollUp()
-    if self.activeIndex - 1 >= 1 then
-        self.activeIndex = self.activeIndex - 1
-    else
-        self.activeIndex = #self.items
-    end
+function SetupMenu:scrollUp()
+   if self.activeIndex - 1 >= 1 then
+      self.activeIndex = self.activeIndex - 1
+   else
+      self.activeIndex = #self.items
+   end
 end
 
-function menu:scrollDown()
-    if self.activeIndex + 1 <= #self.items then
-        self.activeIndex = self.activeIndex + 1
-    else
-        self.activeIndex = 1
-    end
+function SetupMenu:scrollDown()
+   if self.activeIndex + 1 <= #self.items then
+      self.activeIndex = self.activeIndex + 1
+   else
+      self.activeIndex = 1
+   end
 end
 
--- тут изменение параметра в меньшую стророну
-function menu:leftPressed()
-    local item = self.items[self.activeIndex]
-    if item.onleft then
-        item.content, item.isfirst, item.islast = item.onleft()
-        if not item.isfirst then
-            item.isfirst = false
-        end
-        if not item.islast then
-            item.islast = false
-        end
-        item.leftPressedKey = true
-    end
+
+function SetupMenu:leftPressed()
+   local item = self.items[self.activeIndex]
+   if item.onleft then
+      item.content, item.isfirst, item.islast = item.onleft()
+      if not item.isfirst then
+         item.isfirst = false
+      end
+      if not item.islast then
+         item.islast = false
+      end
+      item.leftPressedKey = true
+   end
 end
 
-function menu:leftReleased()
-    local item = self.items[self.activeIndex]
-    item.leftPressedKey = false
+function SetupMenu:leftReleased()
+   local item = self.items[self.activeIndex]
+   item.leftPressedKey = false
 end
 
--- тут изменение параметра в большую строну
-function menu:rightPressed()
-    local item = self.items[self.activeIndex]
-    if item.onright then
-        item.content, item.isfirst, item.islast = item.onright()
-        if not item.isfirst then
-            item.isfirst = false
-        end
-        if not item.islast then
-            item.islast = false
-        end
-        item.rightPressedKey = true
-    end
+
+function SetupMenu:rightPressed()
+   local item = self.items[self.activeIndex]
+   if item.onright then
+      item.content, item.isfirst, item.islast = item.onright()
+      if not item.isfirst then
+         item.isfirst = false
+      end
+      if not item.islast then
+         item.islast = false
+      end
+      item.rightPressedKey = true
+   end
 end
 
-function menu:rightReleased()
-    local item = self.items[self.activeIndex]
-    item.rightPressedKey = false
+function SetupMenu:rightReleased()
+   local item = self.items[self.activeIndex]
+   item.rightPressedKey = false
 end
 
--- целевая задача: рисовка одной менюшки, в центре экрана, с выравниманием 
--- по центру экрана
-function menu:draw()
 
-    local y0 = (h - #self.items * self.font:getHeight()) / 2 
-    local w, h = g.getDimensions()
-    local y = y0
 
-    local oldfont = g.getFont()
-    g.setFont(self.font)
+function SetupMenu:draw()
 
-    local oldLineWidth = g.getLineWidth()
-    g.setLineWidth(self.cursorLineWidth)
 
-    local leftMarker, rightMarker = "<< ", " >>"
-    local leftMarkerColor, rightMarkerColor
+   local w, h = g.getDimensions()
+   local y0 = (h - #self.items * self.font:getHeight()) / 2
+   local y = y0
 
-    --self.rects = {}
-    for k, v in pairs(self.items) do
-        local leftMarkerColor = v.leftPressedKey and self.activeMarkerColor 
-            or (v.isfirst and inactiveMarkerColor or self.markerColor)
-        local rightMarkerColor = v.rightPressedKey and self.activeMarkerColor 
-            or (v.islast and inactiveMarkerColor or self.markerColor)
+   local oldfont = g.getFont()
+   g.setFont(self.font)
 
-        local text = ""
+   local oldLineWidth = g.getLineWidth()
+   g.setLineWidth(self.cursorLineWidth)
 
-        if v.onleft then
-            text = leftMarker
-        end
-        for _, p in pairs(v.content) do
-            if type(p) == "string" then
-                text = text .. p
-            end
-        end
-        if v.onright then
-            text = text .. rightMarker
-        end
+   local leftMarker, rightMarker = "<< ", " >>"
 
-        local textWidth = g.getFont():getWidth(text)
-        local x0 = (w - textWidth) / 2
-        local x = x0
 
-        if v.onleft then
-            g.setColor(leftMarkerColor)
-            g.print(leftMarker, x, y)
+   for k, v in ipairs(self.items) do
+      local leftMarkerColor = v.leftPressedKey and self.activeMarkerColor or
+      (v.isfirst and self.inactiveMarkerColor or self.markerColor)
+      local rightMarkerColor = v.rightPressedKey and self.activeMarkerColor or
+      (v.islast and self.inactiveMarkerColor or self.markerColor)
 
-            local width = g.getFont():getWidth(leftMarker)
-            v.leftRect = { x = x, y = y, w = width,
-                h = g.getFont():getHeight(), k = k}
-            
-            if v.leftBorder then
-                g.setColor(self.cursorColor)
-                g.rectangle("line", v.leftRect.x, v.leftRect.y, v.leftRect.w,
-                    v.leftRect.h)
-            end
+      local text = ""
 
-            x = x + width
-        end
-        local xLeft = x
-        g.setColor(self.color)
-        for _, p in pairs(v.content) do
-            if type(p) == "table" then
-                g.setColor(p)
-            elseif type(p) == "string" then
-                g.print(p, x, y)
-                x = x + g.getFont():getWidth(p)
-            else
-                error("Unexpected type of value.")
-            end
-        end
-        local xRight = x
-        if v.onright then
-            g.setColor(rightMarkerColor)
-            g.print(rightMarker, x, y)
-            local width = g.getFont():getWidth(rightMarker)
-            v.rightRect = { x = x, y = y, w = width,
-                h = g.getFont():getHeight(), k = k}
+      if v.onleft then
+         text = leftMarker
+      end
+      for _, p in ipairs(v.content) do
+         if type(p) == "string" then
+            text = text .. p
+         end
+      end
+      if v.onright then
+         text = text .. rightMarker
+      end
 
-            if v.rightBorder then
-                g.setColor(self.cursorColor)
-                g.rectangle("line", v.rightRect.x, v.rightRect.y, v.rightRect.w,
-                    v.rightRect.h)
-            end
+      local textWidth = g.getFont():getWidth(text)
+      local x0 = (w - textWidth) / 2
+      local x = x0
 
-            x = x + width
-        end
+      if v.onleft then
+         g.setColor(leftMarkerColor)
+         g.print(leftMarker, x, y)
 
-        -- здесь можно заполнить внутреннюю табличку, содержащую координаты
-        -- всех прямоугольников меню
-        -- плохой код так как он дублируется чуть ниже и заполнения и стирания
-        -- производятся каждый кадр
-        -- x, y, w, h, k
-        v.rect = {x = xLeft, y = y, w = xRight - xLeft, 
-            h = g.getFont():getHeight(), k = k}
+         local width = g.getFont():getWidth(leftMarker)
+         v.leftRect = { x = x, y = y, w = width,
+h = g.getFont():getHeight(), k = k, }
 
-        if k == self.activeIndex then 
-            local oldcolor = {g.getColor()}
+         if v.leftBorder then
             g.setColor(self.cursorColor)
-            g.rectangle("line", xLeft, y, xRight - xLeft, 
-                g.getFont():getHeight())
-            g.setColor(oldcolor)
-        end
+            g.rectangle("line", v.leftRect.x, v.leftRect.y, v.leftRect.w,
+            v.leftRect.h)
+         end
 
-        y = y + self.font:getHeight()
-    end
+         x = x + width
+      end
+      local xLeft = x
+      g.setColor(self.color)
+      for _, p in ipairs(v.content) do
+         if type(p) == "table" then
+            g.setColor(p)
+         elseif type(p) == "string" then
+            g.print(p, x, y)
+            x = x + g.getFont():getWidth(p)
+         else
+            error("Unexpected type of value.")
+         end
+      end
+      local xRight = x
+      if v.onright then
+         g.setColor(rightMarkerColor)
+         g.print(rightMarker, x, y)
+         local width = g.getFont():getWidth(rightMarker)
+         v.rightRect = { x = x, y = y, w = width,
+h = g.getFont():getHeight(), k = k, }
 
-    g.setLineWidth(oldLineWidth)
-    g.setFont(oldfont)
+         if v.rightBorder then
+            g.setColor(self.cursorColor)
+            g.rectangle("line", v.rightRect.x, v.rightRect.y, v.rightRect.w,
+            v.rightRect.h)
+         end
+
+         x = x + width
+      end
+
+
+
+
+
+
+      v.rect = { x = xLeft, y = y, w = xRight - xLeft,
+h = g.getFont():getHeight(), k = k, }
+
+      if k == self.activeIndex then
+         local oldcolor = { g.getColor() }
+         g.setColor(self.cursorColor)
+         g.rectangle("line", xLeft, y, xRight - xLeft,
+         g.getFont():getHeight())
+         g.setColor(oldcolor)
+      end
+
+      y = y + self.font:getHeight()
+   end
+
+   g.setLineWidth(oldLineWidth)
+   g.setFont(oldfont)
 end
 
-function menu:mousemoved(x, y, dx, dy, istouch)
-    if not self.freeze then
-        for k, v in pairs(self.items) do
+function SetupMenu:mousemoved(x, y, _, _, _)
+   if not self.freeze then
+      for k, v in ipairs(self.items) do
+         local rect = v.rect
+         if pointInRect(x, y, rect.x, rect.y, rect.w, rect.h) then
+            self.activeIndex = k
+         end
+         local leftRect = v.leftRect
+         v.leftBorder = leftRect and pointInRect(x, y, leftRect.x,
+         leftRect.y, leftRect.w, leftRect.h)
+         local rightRect = v.rightRect
+         v.rightBorder = rightRect and pointInRect(x, y, rightRect.x,
+         rightRect.y, rightRect.w, rightRect.h)
+
+         if v.leftBorder or v.rightBorder then
+            self.activeIndex = k
+         end
+
+      end
+   end
+end
+
+function SetupMenu:keypressed(key)
+   if key == "up" or key == "k" then
+      self:scrollUp()
+   elseif key == "down" or key == "j" then
+      self:scrollDown()
+   end
+end
+
+function SetupMenu:mousepressed(x, y, btn, _)
+   if not self.freeze then
+      if btn == 1 then
+         for k, v in ipairs(self.items) do
             local rect = v.rect
             if pointInRect(x, y, rect.x, rect.y, rect.w, rect.h) then
-                self.activeIndex = k
+               self:select()
             end
             local leftRect = v.leftRect
-            v.leftBorder = leftRect and pointInRect(x, y, leftRect.x, 
-            leftRect.y, leftRect.w, leftRect.h)
+            if leftRect and pointInRect(x, y, leftRect.x, leftRect.y,
+               leftRect.w, leftRect.h) and self.activeIndex == k then
+               self:leftPressed()
+               self:leftReleased()
+            end
             local rightRect = v.rightRect
-            v.rightBorder = rightRect and pointInRect(x, y, rightRect.x, 
-            rightRect.y, rightRect.w, rightRect.h)
-
-            if v.leftBorder or v.rightBorder then
-                self.activeIndex = k
+            if rightRect and pointInRect(x, y, rightRect.x, rightRect.y,
+               rightRect.w, rightRect.h) and self.activeIndex == k then
+               self:rightPressed()
+               self:rightReleased()
             end
-
-        end
-    end
+         end
+      end
+   end
 end
 
-function menu:keypressed(key)
-    if key == "up" or key == "k" then
-        self:scrollUp()
-    elseif key == "down" or key == "j" then
-        self:scrollDown()
-    end
-end
 
-function menu:mousepressed(x, y, btn, istouch)
-    if not self.freeze then
-        if btn == 1 then
-            for k, v in pairs(self.items) do
-                local rect = v.rect
-                if pointInRect(x, y, rect.x, rect.y, rect.w, rect.h) then
-                    self:select()
-                end
-                local leftRect = v.leftRect
-                if leftRect and pointInRect(x, y, leftRect.x, leftRect.y,
-                    leftRect.w, leftRect.h) and self.activeIndex == k then
-                    self:leftPressed()
-                    self:leftReleased()
-                end
-                local rightRect = v.rightRect
-                if rightRect and pointInRect(x, y, rightRect.x, rightRect.y,
-                    rightRect.w, rightRect.h) and self.activeIndex == k then
-                    self:rightPressed()
-                    self:rightReleased()
-                end
-            end
-        end
-    end
-end
 
-return setmetatable(menu, { __call = function(cls, ...)
-    return cls.new(...)
-end})
+
+
+return SetupMenu
---
+++ signal_quad_field.lua
@@ -0,0 +1,313 @@
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local assert = _tl_compat and _tl_compat.assert or assert; local ipairs = _tl_compat and _tl_compat.ipairs or ipairs; local math = _tl_compat and _tl_compat.math or math; local table = _tl_compat and _tl_compat.table or table; require("love")
+require("vector")
+require("nbtypes")
+
+local g = love.graphics
+
+ SignalView = {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+local SignalView_mt = {
+   __index = SignalView,
+}
+
+
+
+local function intersection(start1, end1, start2, end2)
+   assert(vector.isvector(start1) and vector.isvector(end1) and
+   vector.isvector(start2) and vector.isvector(end2))
+
+   local dir1 = end1 - start1;
+   local dir2 = end2 - start2;
+
+
+   local a1 = -dir1.y;
+   local b1 = dir1.x;
+   local d1 = -(a1 * start1.x + b1 * start1.y);
+
+   local a2 = -dir2.y;
+   local b2 = dir2.x;
+   local d2 = -(a2 * start2.x + b2 * start2.y);
+
+
+   local seg1_line2_start = a2 * start1.x + b2 * start1.y + d2;
+   local seg1_line2_end = a2 * end1.x + b2 * end1.y + d2;
+
+   local seg2_line1_start = a1 * start2.x + b1 * start2.y + d1;
+   local seg2_line1_end = a1 * end2.x + b1 * end2.y + d1;
+
+
+   if (seg1_line2_start * seg1_line2_end >= 0 or
+      seg2_line1_start * seg2_line1_end >= 0) then
+      return nil
+   end
+
+   local u = seg1_line2_start / (seg1_line2_start - seg1_line2_end);
+
+   return start1 + u * dir1
+end
+
+
+
+
+function SignalView.new(width, soundPack)
+   local self = {
+      iCount = 10,
+      width = width,
+      sounds = {},
+      canvas = nil,
+      borderColor = { 0, 0, 0 },
+      borderLineWidth = 3,
+   }
+
+   local wavePath = "sfx/" .. soundPack
+   for _, v in ipairs(love.filesystem.getDirectoryItems(wavePath)) do
+      table.insert(self.sounds, love.audio.newSource(wavePath .. "/" .. v,
+      "static"))
+   end
+
+   self = setmetatable(self, SignalView_mt)
+
+
+
+   self:setCorner(0, 0)
+   self:resize(self.width)
+   return self
+end
+
+
+
+function SignalView:setCorner(x, y)
+   self.x0, self.y0 = x, y
+end
+
+function SignalView:resize(width)
+   self.width = width
+
+   self.canvas = g.newCanvas(width, width * 6, { msaa = 2 })
+
+
+
+
+
+end
+
+
+
+
+
+function SignalView:draw(xd, yd, type_, color)
+
+
+
+   local border = 1
+   local w, h = self.width - border * 2, self.width - border * 2
+   local x = self.x0 + xd * self.width + border
+   local y = self.y0 + yd * self.width + border
+
+   self.borderColor[4] = color[4]
+   g.setColor(color)
+   local oldWidth = g.getLineWidth()
+   g.setLineWidth(self.borderLineWidth)
+
+
+
+   self[type_](x, y, w, h)
+
+   g.setLineWidth(oldWidth)
+
+   g.setColor({ 1, 1, 1 })
+
+
+end
+
+
+
+
+
+function SignalView:play(index)
+   assert(index <= #self.sounds)
+
+end
+
+function SignalView:quad(x, y, w, h)
+   local delta = 5
+   g.rectangle("fill", x + delta, y + delta, w - delta * 2, h - delta * 2)
+   g.setColor(self.borderColor)
+   g.rectangle("line", x + delta, y + delta, w - delta * 2, h - delta * 2)
+end
+
+function SignalView:circle(x, y, w, h)
+   g.circle("fill", x + w / 2, y + h / 2, w / 2.3)
+   g.setColor(self.borderColor)
+   g.circle("line", x + w / 2, y + h / 2, w / 2.3)
+end
+
+function SignalView:trdown(x, y, w, h)
+   local magic = 2.64
+   local tri = {}
+   local rad = w / 2
+   for i = 1, 3 do
+      local alpha = 2 * math.pi * i / 3
+      local sx = x + w / 2 + rad * math.sin(alpha)
+      local sy = y + h / magic + rad * math.cos(alpha)
+      tri[#tri + 1] = sx
+      tri[#tri + 1] = sy
+   end
+   g.polygon("fill", tri)
+   g.setColor(self.borderColor)
+   g.polygon("line", tri)
+end
+
+function SignalView:trup(x, y, w, h)
+   local magic = 1.64
+   local tri = {}
+   local rad = w / 2
+   for i = 1, 3 do
+      local alpha = math.pi + 2 * math.pi * i / 3
+      local sx = x + w / 2 + rad * math.sin(alpha)
+      local sy = y + h / magic + rad * math.cos(alpha)
+      tri[#tri + 1] = sx
+      tri[#tri + 1] = sy
+   end
+   g.polygon("fill", tri)
+   g.setColor(self.borderColor)
+   g.polygon("line", tri)
+end
+
+
+
+
+function SignalView:calculateIntersections(up, down)
+   local points = {}
+   local p
+
+
+   p = intersection(vector(up[5], up[6]), vector(up[1], up[2]),
+   vector(down[5], down[6]), vector(down[3], down[4]))
+   points[#points + 1] = p.x
+   points[#points + 1] = p.y
+
+
+   p = intersection(vector(up[5], up[6]), vector(up[1], up[2]),
+   vector(down[3], down[4]), vector(down[1], down[2]))
+   points[#points + 1] = p.x
+   points[#points + 1] = p.y
+
+
+   p = intersection(vector(up[3], up[4]), vector(up[5], up[6]),
+   vector(down[3], down[4]), vector(down[1], down[2]))
+   points[#points + 1] = p.x
+   points[#points + 1] = p.y
+
+
+   p = intersection(vector(up[3], up[4]), vector(up[5], up[6]),
+   vector(down[1], down[2]), vector(down[5], down[6]))
+   points[#points + 1] = p.x
+   points[#points + 1] = p.y
+
+
+   p = intersection(vector(up[3], up[4]), vector(up[1], up[2]),
+   vector(down[1], down[2]), vector(down[5], down[6]))
+   points[#points + 1] = p.x
+   points[#points + 1] = p.y
+
+
+   p = intersection(vector(up[3], up[4]), vector(up[1], up[2]),
+   vector(down[3], down[4]), vector(down[5], down[6]))
+   points[#points + 1] = p.x
+   points[#points + 1] = p.y
+
+   return points
+end
+
+function SignalView:trupdown(_, _, w, h)
+   local tri_up, tri_down = {}, {}
+   local rad = w / 2
+   for i = 1, 3 do
+      local alpha = 2 * math.pi * i / 3
+      local sx = w / 2 + rad * math.sin(alpha)
+      local sy = h / 2 + rad * math.cos(alpha)
+      tri_up[#tri_up + 1] = sx
+      tri_up[#tri_up + 1] = sy
+
+      alpha = math.pi + 2 * math.pi * i / 3
+      sx = w / 2 + rad * math.sin(alpha)
+      sy = h / 2 + rad * math.cos(alpha)
+
+      tri_down[#tri_down + 1] = sx
+      tri_down[#tri_down + 1] = sy
+   end
+
+   g.polygon("fill", tri_up)
+   g.polygon("fill", tri_down)
+
+   local points = self:calculateIntersections(tri_up, tri_down)
+
+   local borderVertices = {
+      tri_up[1], tri_up[2],
+      points[1], points[2],
+      tri_down[3], tri_down[4],
+      points[3], points[4],
+      tri_up[5], tri_up[6],
+      points[5], points[6],
+      tri_down[1], tri_down[2],
+      points[7], points[8],
+      tri_up[3], tri_up[4],
+      points[9], points[10],
+      tri_down[5], tri_down[6],
+      points[11], points[12],
+      tri_up[1], tri_up[2],
+      tri_down[3], tri_down[4],
+   }
+
+   local oldcolor = { g.getColor() }
+   g.setColor(self.borderColor)
+   g.polygon("line", borderVertices)
+   g.setColor(oldcolor)
+end
+
+function SignalView:rhombus(x, y, w, h)
+   local delta = 6
+   g.polygon("fill", { x + delta, y + h / 2, x + w / 2, y + h - delta,
+x + w - delta, y + h / 2,
+x + w / 2, y + delta, })
+   g.setColor(self.borderColor)
+   g.polygon("line", { x + delta, y + h / 2, x + w / 2, y + h - delta,
+x + w - delta, y + h / 2,
+x + w / 2, y + delta, })
+end
+
+return {
+   new = SignalView.new,
+}
